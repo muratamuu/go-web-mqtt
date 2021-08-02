79ca26a51459a6c3b63fb2aaaef2e3e026e40f3a
takashi.murata <takashi.murata@example.com>
2021-08-02 16:03:00 +0900

    systemd script

---
+++ makerom.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cemkfw --info "iwasaki app v1.0" --fwver 2 --userfs userfs --output iwasaki.frm
---
+++ updaterom.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cefirm --name /media/emmc/local/debian/root/iwasaki.frm --mode update --area all
